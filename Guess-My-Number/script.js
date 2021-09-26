319a9da5ceb917516e76bc2601dffd0fcd780c28
Elly1130 <xychong-wp20@student.tarc.edu.my>
2021-09-26 12:18:23 +0800

    Guess-My-Number

---
+++ Guess-My-Number/script.js
@@ -0,0 +1,57 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
+
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // When there is no input
+  if (!guess) {
+    displayMessage('⛔ No Number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🍕 Correct Number!');
+    displayNumber(secretNumber);
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) highScore = score;
+    document.querySelector('.highscore').textContent = highScore;
+
+    // When the guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      displayScore(score);
+    } else {
+      displayMessage('❌ You lost the game!');
+      displayScore(0);
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  displayMessage('Start guessing...');
+  document.querySelector('.guess').value = '';
+  displayScore(score);
+  displayNumber('?');
+});
